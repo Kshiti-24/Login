032cdb08d663f5174fc37d4c94f1941d0450ac70
Kshitiz <kshitizagarwa2405@gmail.com>
2022-10-27 02:37:46 +0530

    Changed colors

--- lib/pages/homepage2.dart
+++ lib/pages/homepage2.dart
@@ -27,102 +27,113 @@ class NewHomePage extends StatelessWidget {
         ],
       ),
 
-      body:Center(
-        child:Column(
-          children: [
-            SizedBox(height: 30,),
-            AnimatedTextKit(
-              animatedTexts: [
-                WavyAnimatedText('Welcome',
-                    textStyle: TextStyle(
-                      color: Colors.teal,
-                      fontSize: 30,
-                    )),
-                WavyAnimatedText('Your details',
-                    textStyle: TextStyle(
-                      color: Colors.teal,
-                      fontSize: 30,
-                    )),
-              ],
-              repeatForever: true,
-              onTap: () {
-                print("Tap Event");
-              },
-            ),
-            SizedBox(height: 20,),
-            Text("Email :",),
-            SizedBox(height: 11,),
-            Text(
-              user.email!,
-              style: const TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold
-              ),
-            ),
-            SizedBox(height: 20,),
-            Text(
-              "User Id"
-            ),
-            SizedBox(height: 11,),
-            Text(
-              user.uid,
-              style: const TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold
-              ),
-            ),
-            SizedBox(height: 30,),
-            InkWell(
-              onTap: () {
-                Navigator.pushNamed(context, MyRoutes.drawerRoute);
-              },
-              child: Text("Check Drawer",
-                style: const TextStyle(
-                  color: Colors.indigo,
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold
+      body:SingleChildScrollView(
+        child:Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [Colors.limeAccent,Colors.teal]
+              )
+          ),
+          child: Center(
+            child: Column(
+              children: [
+                SizedBox(height: 30,),
+                AnimatedTextKit(
+                  animatedTexts: [
+                    WavyAnimatedText('Welcome',
+                        textStyle: TextStyle(
+                          color: Colors.teal,
+                          fontSize: 30,
+                        )),
+                    WavyAnimatedText('Your details',
+                        textStyle: TextStyle(
+                          color: Colors.teal,
+                          fontSize: 30,
+                        )),
+                  ],
+                  repeatForever: true,
+                  onTap: () {
+                    print("Tap Event");
+                  },
                 ),
-              ),
-            ),
+                SizedBox(height: 20,),
+                Text("Email :",),
+                SizedBox(height: 11,),
+                Text(
+                  user.email!,
+                  style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+                SizedBox(height: 20,),
+                Text(
+                  "User Id"
+                ),
+                SizedBox(height: 11,),
+                Text(
+                  user.uid,
+                  style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+                SizedBox(height: 30,),
+                InkWell(
+                  onTap: () {
+                    Navigator.pushNamed(context, MyRoutes.drawerRoute);
+                  },
+                  child: Text("Check Drawer",
+                    style: const TextStyle(
+                      color: Colors.indigo,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ),
 
-            AnimatedTextKit(
-              animatedTexts: [
-                RotateAnimatedText('Eat',
-                    textStyle: TextStyle(
-                        letterSpacing: 3,
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.orange)),
-                RotateAnimatedText('Sleep',
-                    textStyle: TextStyle(
-                        letterSpacing: 3,
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.orange)),
-                RotateAnimatedText(
-                  'Code',
-                  textStyle: TextStyle(
-                      letterSpacing: 3,
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.orange
-                  ),
-                ),
-                RotateAnimatedText(
-                  'Repeat',
-                  textStyle: TextStyle(
-                      letterSpacing: 3,
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.orange
-                  ),
+                AnimatedTextKit(
+                  animatedTexts: [
+                    RotateAnimatedText('Eat',
+                        textStyle: TextStyle(
+                            letterSpacing: 3,
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.orange)),
+                    RotateAnimatedText('Sleep',
+                        textStyle: TextStyle(
+                            letterSpacing: 3,
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.orange)),
+                    RotateAnimatedText(
+                      'Code',
+                      textStyle: TextStyle(
+                          letterSpacing: 3,
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.orange
+                      ),
+                    ),
+                    RotateAnimatedText(
+                      'Repeat',
+                      textStyle: TextStyle(
+                          letterSpacing: 3,
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.orange
+                      ),
+                    ),
+                  ],
+                  isRepeatingAnimation: true,
+                  totalRepeatCount: 100,
+                  pause: Duration(milliseconds: 1000),
                 ),
               ],
-              isRepeatingAnimation: true,
-              totalRepeatCount: 100,
-              pause: Duration(milliseconds: 1000),
             ),
-          ],
+          ),
         ),
       ),
       drawer: MyDrawer(),
