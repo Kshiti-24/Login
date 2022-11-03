9a3565aef6a16b31913fcb718d631501845337f1
Kshitiz <kshitizagarwa2405@gmail.com>
2022-11-03 22:20:43 +0530

    Final Code

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,6 @@ final navigatorKey = GlobalKey<NavigatorState>();
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
